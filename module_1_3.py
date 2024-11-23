659fc2c9d759bf094e096b7a9e8926f06b085a7b
Евгений <97760315+Evgen9922@users.noreply.github.com>
2024-11-23 14:30:42 +0300

    Add files via upload
    Python. Домашние задание по модулю 1

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+from operator import truediv
+
+print('Домашнее зададние, модуль 1:')
+name = 'Евгений'
+print('name = ', name)
+age = 43
+print('age = ',age)
+age = age + 100
+print('Новое значение "age" = ',age)
+is_student = True
+print('Значение переменной "is_student" = ',is_student)
+
